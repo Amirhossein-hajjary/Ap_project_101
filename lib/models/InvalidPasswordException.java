1c03738396ad4deaaa0c9d53b5c533290b01e64b
Ali Ayoobzadeh <aliayoobzadeh2006@gmail.com>
2026-06-28 06:51:14 -0700

    behinehsazi

--- lib/models/InvalidPasswordException.java
+++
@@ -1,9 +0,0 @@
-package models;
-public class InvalidPasswordException extends Exception {
-    public InvalidPasswordException() {
-        super("Invalid password provided");
-    }
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
